fac8dd99b75b8ce38c1c6a78b899bf1aa68fa75f
iderissedom <iderisse.dom@optis.be>
2026-02-23 15:01:33 +0100

    file

--- src/main/java/com/example/demo/FileController.java
+++ src/main/java/com/example/demo/FileController.java
@@ -1,19 +1,21 @@
 package com.example.demo;
 
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
     @GetMapping
-    public FileSystemResource getFile() throws IOException {
-        var file = new ClassPathResource("test.txt").getFile();
-        return new FileSystemResource(file);
+    public ResponseEntity<Resource> getFile() {
+        Resource resource = new ClassPathResource("test.txt");
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"test.txt\"")
+                .body(resource);
     }
 }
